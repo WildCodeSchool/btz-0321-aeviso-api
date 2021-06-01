269fe3927284e5799afea106f7da53322d1daa42
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-01 18:26:59 +0200

    Users CRUD ok

--- src/api/users.js
+++ src/api/users.js
@@ -13,8 +13,8 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const result = usersExemple.filter((user) => user.id === +req.params.id);
-  if (result.length) res.status(200).json(result);
+  const result = usersExemple.find((user) => user.id === +req.params.id);
+  if (result) res.status(200).json(result);
   else
     res.status(404).json({
       message: "No user found",
@@ -28,7 +28,44 @@ router.post("/", (req, res) => {
     ...req.body,
   };
   initial.push(newUser);
-  res.status(200).json(initial[initial.length - 1]);
+  res.status(201).json(initial[initial.length - 1]);
+});
+
+router.put("/:id", (req, res) => {
+  const id = +req.params.id;
+  const index = usersExemple.indexOf(
+    usersExemple.find((user) => user.id === id)
+  );
+  if (index >= 0) {
+    let elementToUpdate = usersExemple.find((user) => user.id === id);
+    elementToUpdate = {
+      id,
+      ...req.body,
+    };
+    usersExemple.splice(index, 1, elementToUpdate);
+    res.status(200).json({
+      message: "User updated",
+    });
+  } else
+    res.status(404).json({
+      message: "No user found",
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = +req.params.id;
+  const index = usersExemple.indexOf(
+    usersExemple.find((user) => user.id === id)
+  );
+  if (index >= 0) {
+    usersExemple.splice(index, 1);
+    res.status(200).json({
+      message: "User deleted",
+    });
+  } else
+    res.status(404).json({
+      message: "No user found",
+    });
 });
 
 module.exports = router;
