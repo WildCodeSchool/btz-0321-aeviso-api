f8dbd9646c55d0fced45d2913e6de690114d88ac
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-06-01 22:21:54 +0200

    Route Get - Orgnanizations

--- src/api/organizations.js
+++ src/api/organizations.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const organizations = require("./dev/organizationsExample");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  const org = organizations;
+  if (org.length) res.status(200).json(org);
+  else res.status(404).json({ message: "Organizations not found" });
+});
+
+router.get("/:id", (req, res) => {
+  const org = organizations.filter((cie) => cie.id === +req.params.id);
+  if (org.length) res.status(200).json(org);
+  else
+    res.status(404).json({ message: "The organization have not been found" });
+});
+
+router.get("/:name", (req, res) => {
+  const org = organizations.filter((cie) => cie.id === +req.params.name);
+  if (org.length) res.status(200).json(org);
+  else
+    res.status(404).json({ message: "The organization have not been found" });
+});
 
 module.exports = router;
