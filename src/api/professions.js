9ed2457670e6f86359afb8b4d86bd40b52cad16e
Benjaminlavigneducadet <benjamin.lavigneducadet@hotmail.fr>
2021-06-02 15:33:19 +0200

    creat profession ok

--- src/api/professions.js
+++ src/api/professions.js
@@ -5,11 +5,29 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.status(200).json(professionsExample);
-
-
-
-
 });
 
+
+router.get("/:id",(req, res) => {
+    const id = +req.params.id;
+    const result = professionsExample.find((profession) => profession.id === id);
+    if (result) res.status(200).json(result);
+    else
+    res.status(404).json({
+        message: "No user found",
+    });
+   
+});
+
+router.post("/", (req, res) => {
+    const initial= professionsExample; 
+    const newProfession = {
+        id: [...initial].pop().id + 1,
+        ...req.body,
+    };
+    initial.push(newProfession);
+    res.status(201).json(initial[initial.length -1]);
+
+});
 
 module.exports = router;
