fa1fbc79cf2e011a3d50c9b501df2692d53867e5
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-06-02 14:44:11 +0200

    Routes Post companies done

--- src/api/companies.js
+++ src/api/companies.js
@@ -4,15 +4,29 @@ const companies = require("./dev/companiesExample");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const org = companies;
-  if (org.length) res.status(200).json(org);
+  if (companies.length) res.status(200).json(companies);
   else res.status(404).json({ message: "companies not found" });
 });
 
 router.get("/:id", (req, res) => {
-  const org = companies.filter((cie) => cie.id === +req.params.id);
-  if (org.length) res.status(200).json(org);
+  companies.filter((cie) => cie.id === +req.params.id);
+  if (companies.length) res.status(200).json(companies);
   else res.status(404).json({ message: "The company have not been found" });
+});
+
+router.post("/", (req, res) => {
+  const { name, zip_code, city, status_id } = req.body;
+  if (!name || !zip_code || !city) {
+    return res.status(400).json({ message: "Please enter the right datas!" });
+  }
+  companies.push({
+    id: companies.length + 1,
+    name,
+    zip_code,
+    city,
+    status_id: status_id || null,
+  });
+  res.status(201).json(companies[companies.length - 1]);
 });
 
 module.exports = router;
