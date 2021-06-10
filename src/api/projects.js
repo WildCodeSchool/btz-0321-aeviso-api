640fcc9d02c5383d39ef1b183915729bab1b79c4
Thomas <barrialthomas@gmail.com>
2021-06-10 16:53:58 +0200

    Init prisma on road GetAll, GetOne, post

--- src/api/projects.js
+++ src/api/projects.js
@@ -1,32 +1,57 @@
 const express = require('express');
 const projetsExample = require('./dev/projectsExamples.json');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json(projetsExample);
+  prisma.project
+    .findMany()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
-router.get('/:id', (req, res) => {
-  const projectID = +req.params.id;
-
-  const result = projetsExample.find((project) => project.id === projectID);
-
-  if (result) {
-    res.status(200).json(result);
-  } else {
-    res.status(404).json({ message: 'project not found' });
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await prisma.project.findUnique({
+      where: {
+        id,
+      },
+    });
+    res.status(200).json(project);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 router.post('/', (req, res) => {
-  const init = projetsExample;
-  const newProjects = {
-    id: [...init].pop().id + 1,
-    ...req.body,
-  };
-  init.push(newProjects);
-  res.status(201).json(init[init.length - 1]);
+  const { name, description, code, companieId } = req.body;
+
+  prisma.project
+    .create({
+      data: {
+        name,
+        description,
+        code,
+        company: {
+          connect: {
+            id: companieId,
+          },
+        },
+      },
+    })
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
