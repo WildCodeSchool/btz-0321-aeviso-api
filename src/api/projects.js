589973d5fb9577f637733c914124da19f50b7c33
Thomas <barrialthomas@gmail.com>
2021-06-11 16:31:24 +0200

    connect prisma client

--- src/api/projects.js
+++ src/api/projects.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const projetsExample = require('./dev/projectsExamples.json');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../../prismaClient');
 
 const router = express.Router();
 
