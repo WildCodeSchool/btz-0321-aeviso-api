c9b9554e851d8ef0440b66d3a0e98a0b1302661a
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-13 15:55:29 +0200

    fix put method bug in users route

--- src/api/users.js
+++ src/api/users.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const usersExample = require("./dev/usersExample");
+const express = require('express');
+const usersExample = require('./dev/usersExample');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json(usersExample);
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   const result = usersExample.find((user) => user.id === +req.params.id);
   if (result) res.status(200).json(result);
   else
     res.status(404).json({
-      message: "No user found",
+      message: 'No user found',
     });
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const initial = usersExample;
   const newUser = {
     id: [...initial].pop().id + 1,
@@ -26,26 +26,29 @@ router.post("/", (req, res) => {
   res.status(201).json(initial[initial.length - 1]);
 });
 
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
+  const { firstname, lastname, email, role, profession } = req.body;
   const id = +req.params.id;
-  const index = usersExample.indexOf(
-    usersExample.find((user) => user.id === id)
-  );
+  const index = usersExample.findIndex((user) => user.id === +req.params.id);
   if (index >= 0) {
-    let elementToUpdate = usersExample.find((user) => user.id === id);
+    let elementToUpdate = usersExample[index];
     elementToUpdate = {
       id,
-      ...req.body,
+      firstname: firstname || elementToUpdate.firstname,
+      lastname: lastname || elementToUpdate.lastname,
+      email: email || elementToUpdate.email,
+      role: role || elementToUpdate.role,
+      profession: profession || elementToUpdate.profession,
     };
     usersExample.splice(index, 1, elementToUpdate);
     res.status(200).json(elementToUpdate);
   } else
     res.status(404).json({
-      message: "No user found",
+      message: 'No user found',
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   const id = +req.params.id;
   const index = usersExample.indexOf(
     usersExample.find((user) => user.id === id)
@@ -53,11 +56,11 @@ router.delete("/:id", (req, res) => {
   if (index >= 0) {
     usersExample.splice(index, 1);
     res.status(204).json({
-      message: "User deleted",
+      message: 'User deleted',
     });
   } else
     res.status(404).json({
-      message: "No user found",
+      message: 'No user found',
     });
 });
 
