89aad6ce2e8887550badb20cb81f380b7e5edd30
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-15 20:52:15 +0200

    Update jobs tests

--- src/api/jobs/controllers/getAll.js
+++ src/api/jobs/controllers/getAll.js
@@ -2,8 +2,8 @@ const prisma = require("../../../../prismaClient");
 
 const getAll = async (req, res, next) => {
   try {
-    const job = await prisma.job.findMany();
-    res.status(200).json(job);
+    const jobs = await prisma.job.findMany();
+    res.status(200).json(jobs);
   } catch (error) {
     res.status(400);
     next(error);
