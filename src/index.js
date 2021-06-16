80801a247f1d56116672007b724f823a9aa19368
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-06-16 11:45:11 +0200

    Rollback index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
