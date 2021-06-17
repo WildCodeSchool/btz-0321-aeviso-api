fa2b9dbca56c92932b02ef45ba663c64463f765e
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-06-17 14:30:07 +0200

    Creating routes for all projects from one company & one project from one company

--- src/api/companies/routes.js
+++ src/api/companies/routes.js
@@ -9,11 +9,16 @@ const post = require("./controllers/post");
 const put = require("./controllers/put");
 const deleteCompany = require("./controllers/deleteCompany");
 const { companySchema, companySchemaEdit } = require("../../schemas");
+const getProjectsFromCompany = require("./controllers/getProjectsFromCompany");
+const getOneProjectFromCompany = require("./controllers/getOneProjectFromCompany");
 
 router.get("/", getAll);
 router.get("/:id", getOne);
 router.post("/", bodyValidator(companySchema), post);
 router.put("/:id", bodyValidator(companySchemaEdit), put);
 router.delete("/:id", deleteCompany);
+
+router.get("/:id/projects", getProjectsFromCompany);
+router.get("/:id/projects/:projectId", getOneProjectFromCompany);
 
 module.exports = router;
