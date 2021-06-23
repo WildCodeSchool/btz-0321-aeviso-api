c2a26ac819884761a57140c72714c968d162f971
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-23 12:57:06 +0200

    fix /companies/:id/projects route

--- src/api/companies/controllers/getProjectsFromCompany.js
+++ src/api/companies/controllers/getProjectsFromCompany.js
@@ -3,11 +3,13 @@ const prisma = require("../../../../prismaClient");
 const getProjectsFromCompany = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const allProjects = await prisma.company.findUnique({
-      where: {
-        id,
-      }.projects(),
-    });
+    const allProjects = await prisma.company
+      .findUnique({
+        where: {
+          id,
+        },
+      })
+      .projects();
     res.status(200).json(allProjects);
   } catch (e) {
     res.status(404);
