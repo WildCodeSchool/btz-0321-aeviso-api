17bd8ca5f0d8c2f3de560e7b8bd714549f930412
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-23 16:18:52 +0200

    Fix try catch error

--- src/api/projects/controllers/getUsers.js
+++ src/api/projects/controllers/getUsers.js
@@ -1,40 +1,45 @@
 const prisma = require("../../../../prismaClient");
 
-const getUsers = async (req, res) => {
+const getUsers = async (req, res, next) => {
   const { id } = req.params;
   const { start, end } = req.query;
-  const { users } = await prisma.project.findUnique({
-    where: {
-      id,
-    },
-    select: {
-      users: {
-        select: {
-          id: true,
-          firstName: true,
-          lastName: true,
-          weeklyBasis: true,
-          job: {
-            select: {
-              label: true,
-            },
-          },
-          records: {
-            where: {
-              projectId: id,
-              date: {
-                gt: start,
-                lt: end,
+  try {
+    const { users } = await prisma.project.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        users: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            weeklyBasis: true,
+            job: {
+              select: {
+                label: true,
               },
             },
-            select: {
-              id: true,
+            records: {
+              where: {
+                projectId: id,
+                date: {
+                  gt: start,
+                  lt: end,
+                },
+              },
+              select: {
+                id: true,
+              },
             },
           },
         },
       },
-    },
-  });
-  return res.status(200).json(users);
+    });
+    return res.status(200).json(users);
+  } catch (e) {
+    res.status(500);
+    return next(e);
+  }
 };
 module.exports = getUsers;
