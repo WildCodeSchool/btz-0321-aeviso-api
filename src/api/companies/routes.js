2a9c44d6c3c63416c6a2e63c8f9a323cb98f4616
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-06-23 14:06:07 +0200

    Adding route to get /companies/:id/admin --> if role admin send request

--- src/api/companies/routes.js
+++ src/api/companies/routes.js
@@ -11,6 +11,8 @@ const deleteCompany = require("./controllers/deleteCompany");
 const { companySchema, companySchemaEdit } = require("../../schemas");
 const getProjectsFromCompany = require("./controllers/getProjectsFromCompany");
 
+const getAdmin = require("./controllers/admin");
+
 /**
  * A company (with id for output display)
  * @typedef {object} DisplayCompany
@@ -36,5 +38,7 @@ router.put("/:id", bodyValidator(companySchemaEdit), put);
 router.delete("/:id", deleteCompany);
 
 router.get("/:id/projects", getProjectsFromCompany);
+
+router.get("/:id/admin", getAdmin);
 
 module.exports = router;
