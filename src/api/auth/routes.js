77c07c237043f2855e8c5794755ee2b44538a1b4
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-25 15:01:10 +0200

    Delete logout route non used yet

--- src/api/auth/routes.js
+++ src/api/auth/routes.js
@@ -5,9 +5,7 @@ const { authSchema } = require("../../schemas");
 const router = express.Router();
 
 const login = require("./controllers/login");
-const logout = require("./controllers/logout");
 
 router.post("/login", bodyValidator(authSchema), login);
-router.post("/logout", logout);
 
 module.exports = router;
