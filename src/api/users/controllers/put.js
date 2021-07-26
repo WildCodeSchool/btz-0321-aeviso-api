d5fde8dc07b4eef5d8eb22b10475dfb177305bf7
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-26 15:54:54 +0200

    FIX => Superadmin can modify admin password

--- src/api/users/controllers/put.js
+++ src/api/users/controllers/put.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const prisma = require("../../../../prismaClient");
 
 /**
@@ -22,6 +23,8 @@ const put = async (req, res, next) => {
     companyId,
     jobId,
   } = req.body;
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
   try {
     if (companyId) {
@@ -51,7 +54,7 @@ const put = async (req, res, next) => {
         firstName,
         lastName,
         email,
-        password,
+        password: hashedPassword,
         role,
         weeklyBasis,
         company,
